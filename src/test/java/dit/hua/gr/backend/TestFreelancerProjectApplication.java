fa9046dcbd27a86a56484082b9c127cfe91af095
anastasia <anastasiiazerva@gmail.com>
2025-05-16 20:26:15 +0300

    6

---
+++ src/test/java/dit/hua/gr/backend/TestFreelancerProjectApplication.java
@@ -0,0 +1,6 @@
+// @SpringBootApplication // Remove or comment out this annotation
+@Configuration
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class TestFreelancerProjectApplication {
+    // Test configuration beans if needed
+} 
