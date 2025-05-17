4fb10e3c1414701c2f30095ad0cf76400c7e8c76
KostasPapadooo <konstantinospapadogiannis99@gmail.com>
2025-05-17 17:47:30 +0300

    Fix test configuration to use specific Spring Boot configuration class

--- src/test/java/dit/hua/gr/backend/TestFreelancerProjectApplicationTests.java
+++ src/test/java/dit/hua/gr/backend/TestFreelancerProjectApplicationTests.java
@@ -3,7 +3,7 @@ package dit.hua.gr.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BackendApplication.class)
 class TestFreelancerProjectApplicationTests {
 
     @Test
