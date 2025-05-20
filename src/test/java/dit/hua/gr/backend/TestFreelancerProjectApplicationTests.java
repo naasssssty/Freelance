4ef6b40e114df735ae59ade6cd0bc042ca381234
KostasPapadooo <konstantinospapadogiannis99@gmail.com>
2025-05-20 10:56:02 +0300

    Add backend tests for Jenkins pipeline: unit tests, integration tests, and code quality configuration

--- src/test/java/dit/hua/gr/backend/TestFreelancerProjectApplicationTests.java
+++ src/test/java/dit/hua/gr/backend/TestFreelancerProjectApplicationTests.java
@@ -1,10 +1,8 @@
 package dit.hua.gr.backend;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled("Disabled due to multiple @SpringBootConfiguration classes")
 @SpringBootTest(classes = BackendApplication.class)
 class TestFreelancerProjectApplicationTests {
 
