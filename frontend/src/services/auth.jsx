2e6e3047632e6deedfa328ee7ab9fe524751be91
KostasPapadooo <konstantinospapadogiannis99@gmail.com>
2025-06-17 11:55:13 +0300

    login page refresh button fixed

--- frontend/src/services/auth.jsx
+++ frontend/src/services/auth.jsx
@@ -12,7 +12,7 @@ const apiClient = axios.create({
 // Login API call
 export const login = async (credentials) => {
     try {
-        const response = await apiClient.post("/login", credentials);
+        const response = await apiClient.post("/api/login", credentials);
         return response; // Return the entire response object
     } catch (error) {
         throw error.response ? error.response.data : error; // Throw the error message from the server
@@ -22,7 +22,7 @@ export const login = async (credentials) => {
 // Register API call
 export const register = async (userData) => {
     try {
-        const response = await apiClient.post("/register", userData);
+        const response = await apiClient.post("/api/register", userData);
         return response.data; // Return the response data
     } catch (error) {
         throw error.response ? error.response.data : error;
